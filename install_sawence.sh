541dc68d6b91950fe87bc52af6adac5af465bb9d
Sawzall <sigill.sawence@gmail.com>
2025-07-13 18:50:48 -0400

    update config

--- install_sawence.sh
+++ install_sawence.sh
@@ -2,7 +2,7 @@
 #update
 sudo apt update && sudo apt upgrade
 #install nvim > 8.0
-sudo apt install neovim zsh curl git tmux python3-pip
+sudo apt install neovim zsh curl git tmux python3-pip bat
 #install ohmyzsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 #install nvm
@@ -11,3 +11,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
 nvm install --lts
 #install tldr
 sudo apt install tealdeer
+#install thefuck
+pipx install thefuck
+
+source ~/.zshrc
