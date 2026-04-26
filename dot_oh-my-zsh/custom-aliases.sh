9f86d11805cb63db209279c0e1ec60ade2b04a25
Sawzall <sigill.sawence@gmail.com>
2026-04-26 13:59:07 +0200

    update hyprland and waybar configs

--- dot_oh-my-zsh/custom-aliases.sh
+++ dot_oh-my-zsh/custom-aliases.sh
@@ -1,7 +1,5 @@
 alias vi="nvim"
 alias vim="nvim"
-alias zshconfig="chezmoi edit ~/.zshrc"
-alias aliasconfig="chezmoi edit ~/.oh-my-zsh/custom-aliases.sh"
 alias zshreload="source ~/.zshrc"
 alias updateapt="sudo apt update && sudo apt upgrade"
 alias aptupdate="updateapt"
@@ -10,3 +8,10 @@ alias start-ollama-server="ollama serve > /dev/null 2>&1 &"
 alias gitversion="dotnet-gitversion"
 alias runtipi="sudo service docker start ; cd ~/Installed_Apps/runtipi && sudo ./runtipi-cli start"
 alias vimdiff='nvim -d'
+
+# configs
+alias zshconfig="chezmoi edit ~/.zshrc"
+alias aliasconfig="chezmoi edit ~/.oh-my-zsh/custom-aliases.sh"
+alias hyprconfig="chezmoi edit ~/.config/hypr/hyprland.conf"
+alias waybarconfig="chezmoi edit ~/.config/waybar/config"
+
